f5ff3f4fb286866183ec7f83a8596c6d42e25838
Kevin Pilch <me@pilchie.com>
2020-02-21 17:19:53 -0800

    Add SLA Configuration

---
+++ SlaOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyFlow
+{
+    public class SlaOptions
+    {
+        public Dictionary<string, Sla> Repositories { get; set;  }
+
+        public Sla GetForRepo(string repoShortName)
+        {
+            if (!Repositories.TryGetValue("dotnet/" + repoShortName, out var value))
+            {
+                value = Repositories["[Default]"];
+            }
+
+            return value;
+        }
+    }
+
+    public class Sla
+    {
+        public int WarningUnconsumedCommitAge { get; set; }
+        public int FailUnconsumedCommitAge { get; set; }
+    }
+}
